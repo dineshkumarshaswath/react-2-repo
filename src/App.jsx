b8dae83c6caa92d776e9d2e054d839b1a2eaeb92
dineshkumarshaswath <dineshshaswath@gmail.com>
2023-04-19 17:03:51 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import react  from 'react'
+import './App.css'
+
+function App() {
+  const  dine=["dhg f","dyhdug"]
+
+  return (
+    <div className="App">
+      <div>
+
+       <h1>
+        dinesh iam doing good
+        this was mu adu rpifukkltie amd tje tjoig as isra; we 
+       </h1>
+       <p>{dine[0]}</p>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
